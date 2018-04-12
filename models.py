f5addcc8cc6ca162db956ac60e501ffb9e038908
saicharan <saicharan.reddy1@gmail.com>
2018-04-12 04:55:03 -0400

    Added relationships in tables and finalized transaction management

--- models.py
+++ models.py
@@ -35,16 +35,14 @@ class Transaction(Base):
     __tablename__ = 'Transaction'
 
     id = db.Column(db.Integer,primary_key=True)
-    user_id = db.Column(db.String(10))
-    seller_id = db.Column(db.String(10))
-    vegetable_id = db.Column(db.String(60))
+    user_id = db.Column(db.String(10),db.ForeignKey('Users.id'))
+    create_user_id = db.relationship("User", foreign_keys=user_id)
     date = db.Column(db.Date)
     amount = db.Column(db.Integer)
 
-    def __init__(self,user_id,seller_id,vegetable_id,amount):
-        self.user_id = user_id
-        self.seller_id = seller_id
-        self.vegetable_id = vegetable_id
+    def __init__(self,user_id,date,amount):
+        self.user_id=user_id
+        self.date = date
         self.amount = amount
 
 class Wholeseller(Base):
